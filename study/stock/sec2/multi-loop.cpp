99aefe31b172d3c3a4d9c9e405ea2dc408c67718
morimori12 <tigogogo60@gmail.com>
2024-01-28 03:47:12 +0000

    Adding the code of the Issue 17

--- study/stock/sec2/multi-loop.cpp
+++ study/stock/sec2/multi-loop.cpp
@@ -91,3 +91,30 @@ void func() {
 int main() { func(); }
 
 */
+
+#include <iostream>
+#include <vector>
+
+int main() {
+  int N, S;
+  std::cin >> N >> S;
+  std::vector<int> A(N), P(N);
+  for (int i = 0; i < N; i++) {
+    std::cin >> A.at(i);
+  }
+  for (int i = 0; i < N; i++) {
+    std::cin >> P.at(i);
+  }
+
+  int ans = 0;
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (A.at(i) + P.at(j) == S) {
+        ans++;
+      }
+    }
+  }
+
+  std::cout << ans << std::endl;
+}
