69709789437e4470395a8b7181234d092d2ec4da
morimori12 <tigogogo60@gmail.com>
2024-02-16 15:54:57 +0000

    modified

--- study/stock/sec2/recursive_function.cpp
+++ study/stock/sec2/recursive_function.cpp
@@ -1,34 +1,47 @@
-/*
-キーポイント
-・「ある関数の中で同じ関数を呼び出す」ことを再帰呼び出しという
-・再帰を行うような関数を再帰関数という
-・再帰呼び出しを行わずに完了できる処理をベースケースという
-・再帰呼出しを行い、その結果を用いて行う処理のことを再帰ステップという
-・再帰関数の実装方法3ステップ
-・1.「引数」「返り値」「処理内容」を決める
-・2.再帰ステップの実装
-・3.ベースケースの実装
-*/
-/*
-再帰関数→「ある関数の中で同じ関数を呼び出す」関数
-
-例:「0からnまでの総和を計算する関数」
 #include <iostream>
+#include <vector>
 
-int sum(int n) {
-  if (n == 0) {
-    return 0;
+// x番の組織が親組織に提出する枚数を返す
+// childrenは組織の関係を表す2次元配列(参照渡し)
+int count_report_num(std::vector<std::vector<int>> &children, int x) {
+  // ベースケース
+  if (children.at(x).size() == 0) {
+    // 子組織から受け取ることは無いので1枚であることは確定している。
+    return 1;
   }
 
-  int s = sum(n - 1);  // sum関数の中でsum関数を呼び出している
-  return s + n;
-}
-int main() {
-  std::cout << sum(2) << std::endl;  // 0+1+2=3
-  std::cout << sum(3) << std::endl;  // 0 + 1 + 2 + 3 = 6
-  std::cout << sum(10)
-            << std::endl;  // 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
+  // 再帰ステップ
+  int sum = 0;
+  for (int c : children.at(x)) {
+    sum += count_report_num(children, c);
+  }
 
-  return 0;
+  sum += 1;  // x板の組織の報告書の枚数(1枚)を足す
+
+  return sum;
 }
-*/
+
+// これ以降の行は変更しなくてよい
+
+int main() {
+  int N;
+  std::cin >> N;
+
+  std::vector<int> p(N);  // 各組織の親組織を示す配列
+  p.at(0) = -1;  // 0番組織の親組織は存在しないので-1を入れておく
+  for (int i = 1; i < N; i++) {
+    std::cin >> p.at(i);
+  }
+
+  // 組織の関係から2次元配列を作る
+  std::vector<std::vector<int>> children(N);  // ある組織の子組織の番号一覧
+  for (int i = 1; i < N; i++) {
+    int parent = p.at(i);              // i番の親組織の番号
+    children.at(parent).push_back(i);  // parentの子組織一覧にi番を追加
+  }
+
+  // 各組織について、答えを出力
+  for (int i = 0; i < N; i++) {
+    std::cout << count_report_num(children, i) << std::endl;
+  }
+}
