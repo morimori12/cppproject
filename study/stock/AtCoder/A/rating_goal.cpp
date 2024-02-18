33ecc8a446613c3a07fe0cafda17c7b2d401477f
morimori12 <tigogogo60@gmail.com>
2024-02-18 05:28:32 +0000

    追加

---
+++ study/stock/AtCoder/A/rating_goal.cpp
@@ -0,0 +1,14 @@
+/*現在のレーティングがRで,
+次のコンテストでレーティングをちょうどGにしたいと思っています。そのとき,
+高橋君が取るべきパフォーマンスを求めなさい*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b;
+
+  c = 2 * b - a;
+  cout << c << endl;
+}
